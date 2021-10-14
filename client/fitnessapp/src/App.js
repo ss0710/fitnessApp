e603f368433a77421199b58ec96973dbdfbbf310
ss0710 <sudheersid0710@gmail.com>
2021-10-14 12:25:28 +0530

    imported components in APP.js

--- client/fitnessapp/src/App.js
+++ client/fitnessapp/src/App.js
@@ -3,12 +3,24 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-const spiKey = 'yqwt2xy8qgmb';
+import ChannelListContainer from './components/ChannelListContainer';
+import ChannelContainer from './components/ChannelContainer';
+
+const apiKey = 'yqwt2xy8qgmb';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+          <ChannelListContainer
+
+          />
+          <ChannelContainer
+
+          />
+      </Chat>
     </div>
   )
 }
