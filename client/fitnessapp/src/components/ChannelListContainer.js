59c1a298418b27d14edc02069676221ac4bf73e3
ss0710 <sudheersid0710@gmail.com>
2021-10-14 20:48:23 +0530

    added separate import file

--- client/fitnessapp/src/components/ChannelListContainer.js
+++ client/fitnessapp/src/components/ChannelListContainer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { ChannelList, useChatCOntext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
 const ChannelListContainer = () => {
     return (
         <div>
