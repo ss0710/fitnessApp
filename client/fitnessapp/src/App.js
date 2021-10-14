d46b532845dc35c81c3eecca61e730cf8d959c71
ss0710 <sudheersid0710@gmail.com>
2021-10-14 12:03:50 +0530

    imported dependencies

--- client/fitnessapp/src/App.js
+++ client/fitnessapp/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const spiKey = 'yqwt2xy8qgmb';
 
 const App = () => {
   return (
