57723bcbc47a4086008227135986bc5db7f700b1
ss0710 <sudheersid0710@gmail.com>
2021-10-14 22:59:52 +0530

    added input feild and event listeners

--- client/fitnessapp/src/components/ChannelSearch.js
+++ client/fitnessapp/src/components/ChannelSearch.js
@@ -4,9 +4,31 @@ import { useChatContext } from 'stream-chat-react';
 import { SearchIcon } from '../assets';
 
 const ChannelSearch = () => {
+
+    const [query, setQuery] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const onSearch = (event) => {
+        event.preventDefault();
+
+        setLoading(true);
+        setQuery(event.target.value);
+    }
+
     return (
-        <div>
-            
+        <div className="channel-search__container">
+            <div className="channel-search__input__wrapper">
+                <div className="channel-search__input__icon">
+                    <SearchIcon />
+                </div>
+                <input 
+                    className="channel-search__input__text" 
+                    type="search"
+                    placeholder="search" 
+                    value={query} 
+                    onChange={onSearch}
+                />
+            </div>
         </div>
     )
 }
