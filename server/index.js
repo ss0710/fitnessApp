095c5b7c0880a803d70480fd51577084cdaff4d8
ss0710 <sudheersid0710@gmail.com>
2021-10-18 19:38:10 +0530

    added logical comments

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 require('dotenv').config(); // this will allow us to call enviroment variables right inside our node application
 
 app.use(cors()); // Cross origin resource sharing enables the browser to load resources from other domains 
-app.use(express.json()); // 
-app.use(express.urlencoded());
+app.use(express.json()); // express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object.This method is called as a middleware in your application using the code: app.
+app.use(express.urlencoded()); //express.urlencoded() is a method inbuilt in express to recognize the incoming Request Object as strings or arrays. This method is called as a middleware in your application using the code: app.
 
 app.get('/', (req, res) => {
     res.send("hello world");
